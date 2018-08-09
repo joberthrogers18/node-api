6b9c4497621b5dbf6e1a4674390dcbf338596a90
Joberth Rogers <joberth.rogers18@gmail.com>
2018-08-09 14:04:00 -0300

    Adding CRUD methods to manipulate API

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -1,9 +1,14 @@
 'user strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json()); // all content became json
+app.use(bodyParser.urlencoded({extended: false})); //codify the urls
+
 
 const route = router.get('/', (req, res, next)=>{
     res.status(200).send({
@@ -11,6 +16,27 @@ const route = router.get('/', (req, res, next)=>{
         version: "0.0.1"
     });
 });
+
+const create = router.post('/', (req, res, next)=>{
+    res.status(201).send(req.body); //status code 201 for create
+});
+
+const put = router.put('/:id', (req, res, next)=>{ // update
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    }); 
+});
+
+const del = router.delete('/', (req, res, next)=>{
+    res.status(200).send(req.body); //delete
+});
+
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
